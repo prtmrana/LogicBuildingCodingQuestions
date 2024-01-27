5d1b5de09af670bdcce3a205df4a5d1c82a2c7ba
preetam rana <preetamrana1995@gmail.com>
2024-01-27 12:40:20 +0530

    Update Palindrome.java

--- StringsRelatedSolution/Palindrome.java
+++ StringsRelatedSolution/Palindrome.java
@@ -25,3 +25,18 @@ public class Palindrome {
         System.out.println("string is not palindrome");
     }
 }
+
+
+                    //------Using stringbuilder class------------------
+
+                  /*  public class PalindromeCheck {
+                        public static boolean isPalindrome(String str) {
+                            StringBuilder sb = new StringBuilder(str);
+                            return str.equals(sb.reverse().toString());
+                        }
+                    
+                        public static void main(String[] args) {
+                            String str = "madam";
+                            System.out.println(isPalindrome(str)); // Output: true
+                        }
+                    }*/
